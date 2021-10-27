bae2faa55f308eac26260ca016a2b24f12c1207a
aveusalex <alexvictor_silva@hotmail.com>
2021-10-27 01:09:33 -0300

    Versao funcional que busca a solucao otima

--- NãoRecursivo/main.c
+++ NãoRecursivo/main.c
@@ -3,7 +3,6 @@
 
 #define MAX 105
 
-
 // função para remover as músicas que já foram inseridas no cartucho que acabou de rodar.
 void removeMusicas(int (*musComponentes)[50], int (*duracaoMusicas)[MAX], int N, int (*NcadaCartucho)[3], int cart){
     int abandonaLaco = 0;  // Essa variável é para saber qual valor
@@ -53,28 +52,18 @@ void verificaCartucho(int tabela_PD[3][MAX][55], int (*musicasComponentes)[50], 
     (*musicasComponentes)[counter] = negativo;
 }
 
-int main() {
-    int K = 0, N = 0;  // declarando variável quantidade de cartuchos e músicas.
-    int sizeCartuchos[] = {0, 0, 0};  // inicializando a lista referente à capacidade dos cartuchos.
-    int j = 0, resultado = 0;  // declarando o índice j (coluna) da tabela.
+int pd(int K, int N, int (*sizeCartuchos)[3], int (*duracaoMusicas)[MAX]) {
+    int j, resultado = 0;  // declarando o índice j (coluna) da tabela.
 
     // arrays
     int musicasComponentes[50];
-    int tabela_PD[3][MAX][55], duracaoMusicas[MAX];  // Cria uma tabela genérica de tamanho máximo baseado no valor máximo das entradas.
+    int tabela_PD[3][MAX][55];  // Cria uma tabela genérica de tamanho máximo baseado no valor máximo das entradas.
     int NcadaCartucho[] = {0, 0, 0};  // Cada cartucho terá um numero de musicas diferente para levar em consideracao na tabela.
 
     // ponteiros para arrays
-    int (*ptr_duracaoMusicas)[MAX] = &duracaoMusicas;
+    int (*ptr_duracaoMusicas)[MAX] = duracaoMusicas;
     int (*ptr_musicasComponentes)[50] = &musicasComponentes;
     int (*ptr_NcadaCartucho)[3] = &NcadaCartucho;
-
-    scanf("%d %d", &N, &K);  // escaneando o N (numero de musicas) e o K (numero de cartuchos)
-    for (int c1 = 1; c1 <= N; c1++){
-        scanf("%d", &duracaoMusicas[c1]);  // escaneando as duracoes das musicas
-    }
-    for (int c2 = 0; c2 < K; c2++) {
-        scanf("%d", &sizeCartuchos[c2]);  // escaneando a capacidade de cada cartucho
-    }
 
     // já colocar a quantidade de musicas disponiveis para o primeiro cartucho
     NcadaCartucho[0] = N;
@@ -85,20 +74,20 @@ int main() {
     for (int cartucho = 0; cartucho < K; cartucho++) {
         // percorremos a tabela de forma top-down, ou seja, de cima para baixo por coluna.
         j = 0;
-        while (j != sizeCartuchos[cartucho] + 1) {
+        while (j != (*sizeCartuchos)[cartucho] + 1) {
             for (int i = 1; i <= NcadaCartucho[cartucho]; i++) {
                 // caso a duracao da música esteja marcada como -1, deve pular para a próxima coluna.
-                if (duracaoMusicas[i] == -1){
+                if ((*duracaoMusicas)[i] == -1){
                     break;
                 }
 
                 // Se a música de duração "duracaoMusicas[i]" couber no cartucho de tamanho "j"...
-                if (duracaoMusicas[i] <= j) {
+                if ((*duracaoMusicas)[i] <= j) {
                     // Se a soma da duração da música atual com a música do cartucho "j - duração atual" for maior que
                     // o elemento da linha superior do mesmo cartucho...
-                    if (tabela_PD[cartucho][i - 1][j - duracaoMusicas[i]] + duracaoMusicas[i] > tabela_PD[cartucho][i - 1][j]) {
+                    if (tabela_PD[cartucho][i - 1][j - (*duracaoMusicas)[i]] + (*duracaoMusicas)[i] > tabela_PD[cartucho][i - 1][j]) {
                         // Então o elemento "tabela_PD[i][j]" recebe a soma mencionada.
-                        tabela_PD[cartucho][i][j] = tabela_PD[cartucho][i - 1][j - duracaoMusicas[i]] + duracaoMusicas[i];
+                        tabela_PD[cartucho][i][j] = tabela_PD[cartucho][i - 1][j - (*duracaoMusicas)[i]] + (*duracaoMusicas)[i];
 
                         // senão, o elemento "tabela_PD[i][j]" recebe o valor anterior do mesmo cartucho.
                     } else tabela_PD[cartucho][i][j] = tabela_PD[cartucho][i - 1][j];
@@ -109,17 +98,107 @@ int main() {
             j += 1;
         }
         // chamada de função para remover as músicas que já foram inseridas no cartucho que acabou de rodar.
-        verificaCartucho(tabela_PD, ptr_musicasComponentes, sizeCartuchos[cartucho], ptr_NcadaCartucho, cartucho, ptr_duracaoMusicas);
+        verificaCartucho(tabela_PD, ptr_musicasComponentes, (*sizeCartuchos)[cartucho], ptr_NcadaCartucho, cartucho, ptr_duracaoMusicas);
         removeMusicas(ptr_musicasComponentes, ptr_duracaoMusicas, N, ptr_NcadaCartucho, cartucho);
 
     }
 
+    // auxiliar auxiliar auxiliar auxiliar auxiliar auxiliar auxiliar auxiliar auxiliar auxiliar auxiliar auxiliar
+    for (int cart = 0; cart<K; cart++) {
+        printf("\n\n\n");
+        printf("\t\t  ");
+        for (int c = 0; c <(*sizeCartuchos)[cart] + 1; c++) {
+            printf("%d \t", c);
+        }
+        printf("\n\n");
+        for (int c2 = 0; c2 <= N; c2++) {
+            printf("%2d: \t\t", c2);
+            for (int c = 0; c <= (*sizeCartuchos)[cart]; c++) {
+                printf("%d \t", tabela_PD[cart][c2][c]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+    // auxiliar auxiliar auxiliar auxiliar auxiliar auxiliar auxiliar auxiliar auxiliar auxiliar auxiliar auxiliar
+
     // Iterar pelo tamanho dos cartuchos e recuperar a informacão correspondente na tabela (elemento N(maior valor da
     // linha) e sizeCartuchos(maior valor da coluna para cada cartucho).
     for (int cart = 0; cart<K; cart++) {
-        resultado += tabela_PD[cart][(NcadaCartucho)[cart]][sizeCartuchos[cart]];
+        resultado += tabela_PD[cart][(NcadaCartucho)[cart]][(*sizeCartuchos)[cart]];
     }
 
-    printf("%d\n", resultado);
-    return 0;
+    return resultado;
+}
+
+void copiaLista(int (*ptr_src)[], int (*ptr_dst)[], int tamanho, int menorIgual){
+    if (menorIgual == 1) {
+        for (int i = 1; i <= tamanho; i++) {
+            (*ptr_dst)[i] = (*ptr_src)[i];
+        }
+    }
+    else{
+        for (int i = 0; i < tamanho; i++) {
+            (*ptr_dst)[i] = (*ptr_src)[i];
+        }
+    }
+}
+
+int main(){
+    // variaveis originais (intocaveis)
+    int K = 0, N = 0;  // declarando variável quantidade de cartuchos e músicas.
+    int sizeCartuchos[] = {0, 0, 0};  // inicializando a lista referente à capacidade dos cartuchos.
+    int duracaoMusicas[MAX];
+
+    // variaveis de fato
+    int K_copia, N_copia;
+    int sizeCartuchos_copia[3];
+    int duracaoMusicas_copia[MAX];
+    int resultados[6];
+
+    scanf("%d %d", &N, &K);  // escaneando o N (numero de musicas) e o K (numero de cartuchos)
+    for (int c1 = 1; c1 <= N; c1++){
+        scanf("%d", &duracaoMusicas[c1]);  // escaneando as duracoes das musicas
+    }
+    for (int c2 = 0; c2 < K; c2++) {
+        scanf("%d", &sizeCartuchos[c2]);  // escaneando a capacidade de cada cartucho
+    }
+
+    if (K == 1){
+        resultados[0] = pd(K, N, &sizeCartuchos, &duracaoMusicas);
+        printf("%d", resultados[0]);
+        return 0;
+    }
+
+    if (K == 2){
+        for (int i=0; i < K; i++){
+            K_copia = K;
+            N_copia = N;
+            copiaLista(&sizeCartuchos, &sizeCartuchos_copia, K_copia, 0);
+            copiaLista(&duracaoMusicas, &duracaoMusicas_copia, N_copia, 1);
+            resultados[i] = pd(K_copia, N_copia, &sizeCartuchos_copia, &duracaoMusicas_copia);
+        }
+        if (resultados[0] > resultados[1]) printf("%d\n", resultados[0]);
+        else printf("%d\n", resultados[1]);
+        return 0;
+    }
+
+    if (K==3){
+        for (int i=0; i < 6; i++){
+            K_copia = K;
+            N_copia = N;
+            copiaLista(&sizeCartuchos, &sizeCartuchos_copia, K_copia, 0);
+            copiaLista(&duracaoMusicas, &duracaoMusicas_copia, N_copia, 1);
+            resultados[i] = pd(K_copia, N_copia, &sizeCartuchos_copia, &duracaoMusicas_copia);
+        }
+        int aux=0;
+        for (int i=0; i<6; i++){
+            if (resultados[i] > aux){
+                aux = resultados[i];
+            }
+        }
+
+        printf("%d", aux);
+        return 0;
+    }
 }
